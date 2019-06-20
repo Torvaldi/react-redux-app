62bd7275260c84a40b0fac4f0941bde6c0cb7750
falia15 <math.1510@hotmail.fr>
2019-06-20 22:28:20 +0200

    rename BlockText component

--- src/components/BlockText/test/BlockText.test.js
+++ src/components/BlockText/test/BlockText.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import BlockText from './BlockText';
+import BlockText from '../BlockText';
 
 configure({ adapter: new Adapter() });
 
