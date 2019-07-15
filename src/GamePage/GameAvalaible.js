9b6feb0052d00218b397b79fb75d4ba0315457d7
Mathilde <math.1510@hotmail.fr>
2019-07-15 22:11:54 +0200

    game page js, print game

--- src/GamePage/GameAvalaible.js
+++ src/GamePage/GameAvalaible.js
@@ -1,40 +1,97 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './css/gameAvalaible.css';
-import { getGameAvalaible } from '../actions/game';
+import { getGameAvalaible, userJoinGame, getUserRunningGame, userReJoinGame } from '../actions/game';
 import GameList from './GameList';
+import { Redirect } from 'react-router-dom';
+
+import './css/gameAvalaible.css';
+import Alert from '../components/Alerte/Alert';
 
 const mapStateToProps = (state, ownProps) => ({...state.game, ...ownProps});
 
 const mapDispatchToProps = (dispatch) => ({
     onGameAvailable:  (token) =>
       getGameAvalaible(dispatch, token),
+    onUserJoinGame: (token, gameId) =>
+      userJoinGame(dispatch, {token, gameId}),
+    onUserRunningGame: (token) =>
+      getUserRunningGame(dispatch, token),
+    onUserReJoinGame: (gameId) =>
+      dispatch(userReJoinGame(gameId))
 });
 
 class GameAvalaible extends Component {
 
+    /**
+     * Initialise the list of the games waiting for player and the game the auth user might have stated
+     */
     componentDidMount = () => {
         const { token } = this.props;
         this.props.onGameAvailable(token);
+        this.props.onUserRunningGame(token);
     }
 
+    /**
+     * Handle event when a user join a game
+     */
+    joinGame = (gameId) => (event) => {
+        event.preventDefault();
+        const { token } = this.props;
+        // check if the user is not already in a game, so they can't rejoin a game twice
+        if(this.props.userRunningGame === false){
+            this.props.onUserJoinGame(token, gameId);
+        }
+    }
+
+    /**
+     * Handle event when a user REjoin a game that have started, so the user can still join the game event if they close the window
+     * @param {*int} gameId
+     */
+    reJoinGame = (gameId) => (event) => {
+        event.preventDefault();
+        this.props.onUserReJoinGame(gameId);
+    }
+
+    /**
+     * Print all waiting for players games
+     * @param {*object} object of game
+     */
     printGameList = (games) => {
         return(
-            <ul>
+            <ul className="gamelist_list">
                 {games.map((game) => {
-                    return <GameList game={game} />;
+                    return <GameList game={game} joinGame={this.joinGame} gameStatus="disable" />;
                 })}
             </ul>
         );
     };
 
+    /**
+     * Print the last game of the user that is still running, so they can REjoin it again
+     * @param {*object} game, unique game object 
+     */
+    printRunningGame = (game) => {
+        return (
+            <section className="runningGame">
+                <Alert type={2} message="You still have a session running !" />
+                <GameList game={game} joinGame={this.reJoinGame} gameStatus="running" />
+            </section>
+        );
+    }
+
     render(){
-        const { games } = this.props;
+        if(this.props.userJoinGame === true){
+            return <Redirect to="game/running" />
+        }
+
+        const { games, userRunningGame, runningGame } = this.props;
+
         return (
          <section className="layout">
             <h1 className="title">Join a game</h1>
+            {userRunningGame === true ? this.printRunningGame(runningGame) : ''}
             <span className="text">Waiting for players</span>
-            {games ? this.printGameList(games) : ' fff' }
+            {games ? this.printGameList(games) : '' }
          </section>
         );
     }
--- src/GamePage/GameList.js
+++ src/GamePage/GameList.js
@@ -1,26 +1,76 @@
 import React from 'react';
+import { getGamelistDateText } from '../helper/date';
+import { getGameStatus } from '../helper/game';
+
 import './css/gameList.css'
-import { getDifferenceCurrentDate } from '../helper/date';
+import Button from '@material-ui/core/Button';
 
-const GameList = ({game : {id, userCreator, timestamp, level, answer, scoreToWin} }) => {
+const GameList = ({
+  joinGame, 
+  gameStatus, 
+  game: { id, creator, timestamp, level, answer, score_to_win, total_player, status}
+  }) => {
+      // get date under the form of "created at x minutes ago"
+      let textCreationDate = getGamelistDateText(timestamp);
+      
+      // dynamic style
+      var block_gamelist = 'block_gamelist';
+      var disabled = false;
+      if(gameStatus == 'disable'){
+        block_gamelist = 'block_gamelist_disable';
+        disabled = true;
+      }
 
-    let date = getDifferenceCurrentDate(timestamp);
+      if(gameStatus == 'running'){
+        var TextStatus = <span className="status_title"> - {getGameStatus(status)}</span>
+      }
 
-    return (
-      <li className="block_gamelist" key={id}>
-          <section className="gamelist_info">
-            <h2>{userCreator.username} 's game</h2>
-            <span className="gamelist_time">Created 
-              <span className="time_detail"> {date.minutes} </span> 
-              minutes and 
-              <span className="time_detail"> {date.seconds} </span>seconds ago
-            </span>
-          </section>
-          <section className="gamelist_config">
-
-          </section>
-      </li>
-    );
+      return(
+        <li className={block_gamelist} key={id}>
+            <section className="gamelist_info">
+              <h2 className="gamelist_title">
+              {creator} 's game
+              {TextStatus ? TextStatus : ''}
+              </h2>
+              <span className="gamelist_time"> 
+                {textCreationDate}
+              </span>
+            </section>
+            <section className="gamelist_config">
+              <article className="config_detail">
+                <span className="config_detail_title">Players</span>
+                <span className="config_detail_data" >{total_player}/10</span>
+              </article>
+              <article className="config_detail">
+                <span className="config_detail_title">Level</span>
+                <span className="config_detail_data" >{level}</span>
+              </article>
+              <article className="config_detail">
+                <span className="config_detail_title">Answers</span>
+                <span className="config_detail_data">{answer}</span>
+              </article>
+              <article className="config_detail">
+                <span className="config_detail_title">Winning Score</span>
+                <span className="config_detail_data">{score_to_win}</span>
+              </article>
+            </section>
+            <section className="gamelist_join">
+              <form onSubmit={joinGame(id)} >
+              <Button 
+              type="submit" 
+              size="medium" 
+              variant="contained" 
+              color="secondary"
+              disabled={disabled}
+              >
+                Join
+              </Button>
+              </form>
+            </section>
+        </li>
+      );
+    
 }
 
-export default GameList;
+
+export default GameList
