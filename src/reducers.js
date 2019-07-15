d244502537c6d5e4f55c9f2ada1d9cd28e4bbc4f
Mathilde <math.1510@hotmail.fr>
2019-07-15 22:11:19 +0200

    add new reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
 import auth from './reducers/auth';
+import game from './reducers/game';
+
 
 export default combineReducers({
     auth,
+    game,
 });
