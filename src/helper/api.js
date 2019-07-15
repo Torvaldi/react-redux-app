1515d67a74055f3aa55e7489fc91b401af20a276
Mathilde <math.1510@hotmail.fr>
2019-07-15 22:09:57 +0200

    add new api route

--- src/helper/api.js
+++ src/helper/api.js
@@ -4,3 +4,19 @@ import { API_URL } from '../config';
 
 export const API_LOGIN = API_URL + 'api/login';
 export const API_REGISTER = API_URL + 'api/register';
+export const API_LOAD_GAME_BY_STATUS = API_URL + 'api/game.index.status';
+export const API_USER_JOIN_GAME = API_URL + 'api/game.store.join';
+export const API_USER_RUNNING_GAME = API_URL + 'api/game.index.running';
+
+// API header
+export const getAuthorizationHeader = (token) => {
+    return {
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, DELETE, PUT',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers' : "content-type, access-control-allow-origin, access-control-allow-credentials, access-control-allow-headers, access-control-allow-methods, Authorization",
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': token,
+    }
+}
