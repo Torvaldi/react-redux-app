b8d5c3f116e92b030440a63dc45e0696c89c097f
Mathilde <math.1510@hotmail.fr>
2019-07-26 16:06:24 +0200

    add event and reducer

--- src/reducers.js
+++ src/reducers.js
@@ -3,9 +3,15 @@ import { combineReducers } from 'redux';
 import token from './reducers/token';
 import auth from './reducers/auth';
 import game from './reducers/game';
+import runningGame from './reducers/runningGame';
+import chat from './reducers/chat';
+import player from './reducers/player'
 
 export default combineReducers({
     auth,
     token,
     game,
+    runningGame,
+    chat,
+    player,
 });
--- src/socket.js
+++ src/socket.js
@@ -4,5 +4,10 @@ import { SOCKET_URL } from './config';
 
 export default io(SOCKET_URL);
 
+// game selection event
 export const NEW_GAME = 'NEW_GAME';
 export const JOIN_GAME = 'JOIN_GAME';
+
+// game event
+export const USER_JOIN_GAME = 'USER_JOIN_GAME';
+export const USER_POST_CHAT = 'USER_POST_CHAT';
