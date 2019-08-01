af9f98612d10a298d5927df41510a783f6e4e12d
Mathilde <math.1510@hotmail.fr>
2019-08-01 15:55:21 +0200

    create main game helpers

--- src/helper/api.js
+++ src/helper/api.js
@@ -9,6 +9,12 @@ export const API_USER_JOIN_GAME = API_URL + 'api/game.store.join';
 export const API_USER_RUNNING_GAME = API_URL + 'api/game.index.running';
 export const API_NEW_GAME = API_URL + 'api/game.store';
 
+
+export const API_USER_GAME = API_URL + 'api/game.user.show';
+export const API_GAME = API_URL + 'api/game.show';
+export const API_GAME_UPDATE_STATUS = API_URL + "api/game.status";
+export const API_GET_ANIMES = API_URL + "api/anime.index";
+
 // API header
 export const getAuthorizationHeader = (token) => {
     return {
@@ -19,5 +25,16 @@ export const getAuthorizationHeader = (token) => {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization': token,
+    }
+}
+
+export const getCrosHeader = () => {
+    return {
+        'Access-Control-Allow-Origin':'*',
+        'mode': 'no-cors',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, DELETE, PUT',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
     }
 }
---
+++ src/helper/mainGame.js
@@ -0,0 +1,27 @@
+
+/**
+ * Source : https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array
+ * @param {} animes 
+ * @param {*} answers 
+ */
+export function getAnimeToGuess(animes, answers){
+    // Shuffle array
+    const shuffled = animes.sort(() => 0.5 - Math.random());
+
+    // Get sub-array of first n (here answers) elements after shuffled
+    let selected = shuffled.slice(0, answers);
+    
+    let animeToGuess = selected[Math.floor(Math.random()*selected.length)];
+    let animeOpening = animeToGuess.openings;
+    let openingToGuess = animeOpening[Math.floor(Math.random()*animeOpening.length)]
+
+    return {
+        animes: selected,
+        animeToGuess,
+        openingToGuess,
+    }
+}
+
+export function getMoeLink(fileName){
+    return 'https://animethemes.moe/video/' + fileName;
+}
