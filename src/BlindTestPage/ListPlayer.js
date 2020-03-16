cc39c4f9821f936d9380c91efb54e2f510ec2d2d
falia15 <math.1510@hotmail.fr>
2020-03-16 18:25:54 +0100

    add update css list player

--- src/BlindTestPage/ListPlayer.js
+++ src/BlindTestPage/ListPlayer.js
@@ -8,6 +8,8 @@ import Player from '../components/Player/Player';
 import Button from '@material-ui/core/Button';
 import './css/listPlayer.css';
 
+import profilePic from './../asset/profilePic.png';
+
 const mapStateToProps = (state, ownProps) => ({...state.player, ...ownProps});
 
 const mapDispatchToProps = (dispatch) => ({});
@@ -38,57 +40,42 @@ class ListPlayer extends React.Component {
     
   }
 
-  printLauchButton() {
-    return(
-      <Button 
-        className="buttonLauchGame" 
-        variant="contained" 
-        color="secondary"
-        onClick={this.props.launchGame}
-      >
-      Launch the game !
-      </Button>
-    );
-  }
-
   /**
    * Print player list with their name and score
    */
   printLeftBar() {
-    const { authUser, game, gameStatus, players } = this.props;
-
-    let status;
-    if(gameStatus === 1) {
-      status = getGameStatus(1);
-    } else if(gameStatus === 2) {
-      status = getGameStatus(2);
-    } else {
-      status = getGameStatus(3);
-    }
+    const { authUser, players } = this.props;
 
     let count = 0;
 
     return(
       <section className="listPlayerLayout">
           <section className="listPlayerInfo">
-            <h1 className="playerTitle">{game.creator} 's game</h1>
-            <span className="playerScoreToWin">Winning score : <span className="playerScoreToWinData">{game.score_to_win}</span></span>
-            <span className="playerLevel">Level : <span className="playerLevelData">{game.level}</span></span>
-            <span className="playerStatus">Status :  <span className="playerStatusData">{status}</span></span>
-            {game.creator === authUser.username && gameStatus === 1 ? this.printLauchButton() : ''}
+
+            <article className="listPlayerInfoUser">
+              <article className="listPlayerInfoUserProfilePicLayout">
+                  <img className="listPlayerInfoUserProfilePic" src={profilePic}></img>
+              </article>
+              <article className="listPlayerInfoUserRight">
+                <h1 className="listPlayerInfoUserUsername">{authUser.username}</h1>
+                <span className="listPlayerInfoUserScore">Score: 0</span>
+                <span className="listPlayerInfoUserRank">#0</span>
+              </article>
+            </article>
+
           </section>
-        <ul className="listPlayer">
           <section className="listPlayerLabel">
             <span className="playerLabel">Players</span>
             <span className="playerScore">Score</span>
           </section>
-          {players.map((player) => {
-            count++;
-            return <Player key={count} player={player} authUser={authUser} />;
-          })}
-        </ul>
+          <ul className="listPlayer">
+            {players.map((player) => {
+              count++;
+              return <Player key={count} player={player} authUser={authUser} count={count} />;
+            })}
+          </ul>
           <Button 
-            variant="contained" 
+            variant="contained" players
             color="secondary"
             size="small"
             onClick={this.props.leaveGame}
