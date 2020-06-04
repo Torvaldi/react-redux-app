f0231c590de370e6f1bd15e376c0240d7ba6a192
falia15 <math.1510@hotmail.fr>
2020-06-04 19:26:34 +0200

    add beta test message on home page

--- client/src/HomePage/Home.js
+++ client/src/HomePage/Home.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Home extends React.Component {
+class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Home
-        </header>
+      <div>
+        <h1>Beta Test</h1>
+        
+
+        <Link to="/login">Login</Link>
+        <br/>
+         <Link to="/register">Register</Link>
+
+
       </div>
     );
   }
